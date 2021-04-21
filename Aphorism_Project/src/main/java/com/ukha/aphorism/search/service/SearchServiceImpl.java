f2f0b5c402215a229997c6eb9de6990f34d16f6b
notebook <dkdkdkdk555@naver.com>
2021-04-21 16:41:40 +0900

    지난 커밋 '카테고리(x) --> Search 탭(O)'

--- Aphorism_Project/src/main/java/com/ukha/aphorism/search/service/SearchServiceImpl.java
+++ Aphorism_Project/src/main/java/com/ukha/aphorism/search/service/SearchServiceImpl.java
@@ -19,7 +19,7 @@ import com.ukha.aphorism.data.Data;
 @Service
 public class SearchServiceImpl implements SearchService{
 
-	//전달받은 keyword에 대한 명언 검색결과를 list로 리턴하는 메소드_2021.04.20
+	//전달받은 keyword에 대한 명언 검색결과를 list로 리턴하는 메소드_2021.04.20..
 	@Override
 	public List<Data> getData(String keyword) {
 		//정규표현식으로 전달된 키워드의 언어 구분
